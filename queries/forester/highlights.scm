4be70678f10b682ea21ca6e0465ef1a0019363c3
Kento Okura <kentookura@gmail.com>
2024-01-04 01:30:37 +0100

    improve syntax highlighting

--- queries/forester/highlights.scm
+++ queries/forester/highlights.scm
@@ -1,5 +1,20 @@
-(ident) @type
-(inline_math) @function
-(display_math) @function
-(text) @variable
-(comment) @comment
+"transclude" @keyword
+"title" @field
+"date" @field
+"taxon" @field
+"ul" @field
+"ol" @field
+"li" @field
+"p" @field
+"code" @field
+"def" @define
+"object" @lsp.type.class
+"scope" @field
+"put" @keyword
+"query" @keyword
+
+(ident) @field
+(addr) @text.underline
+(link_label) @label
+(link_dest) @text.underline @variable
+(external_link) @text.underline
