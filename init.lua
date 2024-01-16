1794952c01b00489ed88e66e39c832005bf16dab
Kento Okura <kentookura@gmail.com>
2024-01-16 12:58:44 +0100

    update test config

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = " "
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -14,18 +16,24 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   {
     dir = "./.", -- change this to line to: "kentookura/neovim",
+    name = "forester.nvim",
     opts = {
-      tree_dirs = { "test/trees" },
+      forests = { "~/glade/notes", "~/forest" }, -- global forest config
+      tree_dirs = { "trees", "test/trees", "notes", "doc" }, -- plugin will check if current directory contains these
       conceal = false,
     },
     config = function(opts)
-      require("forester").setup(opts)
+      local forester = require("forester").setup(opts)
+      vim.keymap.set("n", "<leader>n.", "<cmd>Forester browse<CR>", { silent = true })
     end,
     dependencies = {
+      { "nvim-telescope/telescope.nvim" },
       { "nvim-treesitter/nvim-treesitter" },
       { "nvim-lua/plenary.nvim" },
     },
   },
+  { "nvim-treesitter/nvim-treesitter" },
 })
 
 vim.keymap.set("n", "<leader>t", "<Plug>PlenaryTestFile %")
+vim.keymap.set("n", "<leader>r", "<Plug>Lazy reload forester.nvim")
