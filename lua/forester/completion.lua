d2e9a217e3b490a338b8702880e40b1b53cea36b
Kento Okura <kentookura@gmail.com>
2024-03-05 13:52:58 +0100

    complete addresses

--- lua/forester/completion.lua
+++ lua/forester/completion.lua
@@ -1,3 +1,5 @@
+local forester = require("forester.bindings")
+local util = require("forester.util")
 local source = {}
 
 ---Return whether this source is available in the current context or not (optional).
@@ -28,28 +30,51 @@ end
 
 ---Return trigger characters for triggering completion (optional).
 function source:get_trigger_characters()
-  return { "\\" }
+  return { "\\", "(" }
 end
 
+-- local items = util.map(vim.json.decode(forester.query("all", "doc")[1]), function(tree)
+--   -- return { label = tree.title }
+--   return tree
+-- end)
+-- vim.print(vim.inspect(items))
 ---Invoke completion (required).
----@param params cmp.SourceCompletionApiParams
----@param callback fun(response: lsp.CompletionResponse|nil)
+---
 function source:complete(params, callback)
-  callback({
-    { label = "title" },
-    { label = "author" },
-    { label = "date" },
-    { label = "taxon" },
-    { label = "def" },
-    { label = "import" },
-    { label = "export" },
-    { label = "p" },
-    { label = "strong" },
-    { label = "transclude" },
-    { label = "let" },
-    { label = "code" },
-    { label = "tex" },
-  })
+  local input = string.sub(params.context.cursor_before_line, params.offset - 1)
+  if vim.startswith(input, "(") then
+    local items = {}
+    local trees = vim.json.decode(forester.query("all", "doc")[1])
+    vim.print(vim.inspect(trees))
+    for addr, data in pairs(trees) do
+      -- vim.print(vim.inspect(data))
+      table.insert(items, {
+        filterText = addr .. " " .. data.title,
+        label = data.title .. " (" .. addr .. ")",
+        insertText = addr,
+        documentation = "hello",
+        detail = addr,
+      })
+    end
+    --vim.print(vim.inspect(items))
+    callback({ items = items })
+  elseif vim.startswith(input, "\\") then
+    callback({
+      { label = "title" },
+      { label = "author" },
+      { label = "date" },
+      { label = "taxon" },
+      { label = "def" },
+      { label = "import" },
+      { label = "export" },
+      { label = "p" },
+      { label = "strong" },
+      { label = "transclude" },
+      { label = "let" },
+      { label = "code" },
+      { label = "tex" },
+    })
+  end
 end
 
 ---Resolve completion item (optional). This is called right before the completion is about to be displayed.
